f78a4a9bda86c899e49afd41386dd77936f51236
okikiola94 <71730677+okikiola94@users.noreply.github.com>
2023-03-27 02:56:17 +0100

    feat: created cart and checkout pages

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Homepage from "./components/Homepage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CoursePostingForm from "./Page/Course/Courses";
 import Profile from "./components/Profile/Profile";
-import AdminLogin from './Page/admin/adminLog';
+import AdminLogin from "./Page/admin/adminLog";
+import CartPage from "./Page/AddToCart/AddCart";
+import DisplayCart from "./Page/AddToCart/DisplayCart";
+import BillingPage from "./Page/checkout/checkout";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<Homepage />} />
-          <Route exact path='/course' element={<CoursePostingForm />} />
-          <Route exact path='/profile' element={<Profile />} />
-          <Route path='/adminLog' element={<AdminLogin/>}/>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/course" element={<CoursePostingForm />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route path="/adminLog" element={<AdminLogin />} />
+          <Route path="cart" element={<CartPage />} />
+          <Route path="displaycart" element={<DisplayCart />} />
+          <Route path="checkout" element={<BillingPage />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
